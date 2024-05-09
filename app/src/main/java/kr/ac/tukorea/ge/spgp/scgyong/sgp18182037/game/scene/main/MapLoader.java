43a87f111bedeaa15a94b318a663ae5bb08c9362
Cho Sang Jun <fpdltm456@gmail.com>
2024-05-10 05:30:16 +0900

    차차 필요없는 부분 제거 예정

--- app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/sgp18182037/game/scene/main/MapLoader.java
+++ app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/sgp18182037/game/scene/main/MapLoader.java
@@ -17,29 +17,29 @@ public class MapLoader implements IGameObject {
 
     @Override
     public void update(float elapsedSeconds) {
-        platformX += MapObject.SPEED * elapsedSeconds;
-        while (platformX < Metrics.width) {
-            Platform platform = Platform.get(Platform.Type.RANDOM, platformX, 7); //Metrics.height - 2);
-            platform.addToScene();
-            platformX += platform.getWidth();
-        }
-        itemX += MapObject.SPEED * elapsedSeconds;
-        while (itemX < Metrics.width) {
-            int y = random.nextInt(6) + 1;
-            int count = 3;
-            if (y < 5) {
-                Platform platform = Platform.get(Platform.Type.T_3x1, itemX, y+1);
-                platform.addToScene();
-            } else {
-                count = random.nextInt(5) + 1;
-            }
-            for (int i = 0; i < count; i++) {
-                int y2 = y -= random.nextInt(3);
-                JellyItem jellyItem = JellyItem.get(JellyItem.RANDOM, itemX, y2);
-                jellyItem.addToScene();
-                itemX += jellyItem.getWidth();
-            }
-        }
+//        platformX += MapObject.SPEED * elapsedSeconds;
+//        while (platformX < Metrics.width) {
+//            Platform platform = Platform.get(Platform.Type.RANDOM, platformX, 7); //Metrics.height - 2);
+//            platform.addToScene();
+//            platformX += platform.getWidth();
+//        }
+//        itemX += MapObject.SPEED * elapsedSeconds;
+//        while (itemX < Metrics.width) {
+//            int y = random.nextInt(6) + 1;
+//            int count = 3;
+//            if (y < 5) {
+//                Platform platform = Platform.get(Platform.Type.T_3x1, itemX, y+1);
+//                platform.addToScene();
+//            } else {
+//                count = random.nextInt(5) + 1;
+//            }
+//            for (int i = 0; i < count; i++) {
+//                int y2 = y -= random.nextInt(3);
+//                JellyItem jellyItem = JellyItem.get(JellyItem.RANDOM, itemX, y2);
+//                jellyItem.addToScene();
+//                itemX += jellyItem.getWidth();
+//            }
+//        }
     }
 
     @Override
